17ad4178a5c5cc76998740fce0904d6dbc43eefb
Sam Finch <roadrunnersf@gmail.com>
2020-04-18 18:43:46 +0100

    add redux dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -5,4 +5,7 @@ const rootReducer = combineReducers({
 	notes: notesReducer,
 })
 
-export const store = createStore(rootReducer)
+export const store = createStore(
+	rootReducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__?.()
+)
