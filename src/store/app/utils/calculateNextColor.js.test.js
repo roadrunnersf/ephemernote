ee41bfa181079970215e9e6ae69ae09bce982ec3
Sam Finch <roadrunnersf@gmail.com>
2020-04-21 16:53:19 +0100

    add vscode color theme

--- src/store/app/utils/calculateNextColor.js.test.js
+++ src/store/app/utils/calculateNextColor.js.test.js
@@ -1,8 +1,17 @@
+import { themeColorSchemes } from 'theme'
 import { calculateNextColor } from './calculateNextColor'
 
 describe('calculateNextColor', () => {
 	it('Calculates the correct next color', () => {
-		expect(calculateNextColor('seascape')).toBe('monochrome')
-		expect(calculateNextColor('mint')).toBe('seascape')
+		const colorsList = Object.keys(themeColorSchemes)
+
+		expect(calculateNextColor(colorsList[0])).toBe(colorsList[1])
+		expect(calculateNextColor(colorsList[2])).toBe(colorsList[3])
+
+		const colorsLength = colorsList.length
+		const lastColorScheme = colorsList[colorsLength - 1]
+		const firstColorScheme = colorsList[0]
+
+		expect(calculateNextColor(lastColorScheme)).toBe(firstColorScheme)
 	})
 })
--- src/theme.js
+++ src/theme.js
@@ -13,19 +13,26 @@ export const themeColorSchemes = {
 		background: 'rgb(216,211,208)',
 		tertiary: 'rgb(157,168,168)',
 	},
-	monochrome: {
-		primary: 'rgb(63,63,63)',
-		secondary: 'rgb(168,168,168)',
-		text: 'rgb(242,242,242)',
-		background: 'rgb(211,211,211)',
-		tertiary: 'rgb(3,3,3)',
-	},
 	moroccan: {
 		primary: 'rgb(167,70,45)',
 		secondary: 'rgb(208,115,71)',
 		text: 'rgb(231,196,168)',
 		background: 'rgb(236,238,237)',
 		tertiary: 'rgb(80,41,32)',
+	},
+	vscode: {
+		primary: 'rgb(30,30,30)',
+		secondary: 'rgb(101,141,210)',
+		text: 'rgb(224,213,193)',
+		background: 'rgb(242,242,242)',
+		tertiary: 'rgb(101,185,169)',
+	},
+	monochrome: {
+		primary: 'rgb(63,63,63)',
+		secondary: 'rgb(168,168,168)',
+		text: 'rgb(242,242,242)',
+		background: 'rgb(211,211,211)',
+		tertiary: 'rgb(3,3,3)',
 	},
 }
 
