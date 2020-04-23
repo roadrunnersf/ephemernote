554d9fbc81b564ae5b8e2ec5f85b6a2b812866ad
Sam Finch <roadrunnersf@gmail.com>
2020-04-23 17:26:12 +0100

    Remove commented code from store/app/index

--- src/store/app/index.js
+++ src/store/app/index.js
@@ -3,15 +3,12 @@ import { calculateNextColor } from 'store/app/utils'
 // actions
 
 export const CYCLE_THEME_COLOR = 'Cycle theme color'
-// const SHOW_ADD_NOTE_INPUT = 'Show add note input'
 
 //action creators
+
 export const cycleThemeColor = () => ({
 	type: CYCLE_THEME_COLOR,
 })
-// export const showAddNoteInput = () => ({
-// 	type: SHOW_ADD_NOTE_INPUT,
-// })
 
 // reducer
 
@@ -26,19 +23,7 @@ export const appReducer = (state = initialState, action) => {
 				...state,
 				themeColor: calculateNextColor(state.themeColor),
 			}
-		// case SHOW_ADD_NOTE_INPUT:
-		// 	return {
-		// 		...state,
-		// 		showAddNoteInput: false,
-		// 	}
-		// case HIDE_ADD_NOTE_INPUT:
-		// 	return {
-		// 		...state,
-		// 		showAddNoteInput: true,
-		// 	}
 		default:
 			return state
 	}
 }
-
-// utils
