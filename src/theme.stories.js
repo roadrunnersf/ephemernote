0963b0ad6c32da0856dc49560f9ebbbb4c6c1447
Sam Finch <roadrunnersf@gmail.com>
2020-04-24 16:36:25 +0100

    Add new theme color schemes to storybook

--- src/theme.stories.js
+++ src/theme.stories.js
@@ -1,12 +1,17 @@
 import React from 'react'
 
-import { mintTheme } from 'theme'
+import { themeColorSchemes as theme } from 'theme'
+
+const font = {
+	fontFamily: '"Courier New", Courier, monospace',
+}
 
 const ColoredBox = ({ children, backGroundColor }) => (
 	<div
 		style={{
 			backgroundColor: backGroundColor,
 			display: 'flex',
+			flex: 1,
 			justifyContent: 'center',
 			alignItems: 'center',
 			width: 200,
@@ -17,19 +22,37 @@ const ColoredBox = ({ children, backGroundColor }) => (
 	</div>
 )
 
-export default { title: 'Mint Theme' }
+export default { title: 'Theme' }
 
-export const colors = () => (
-	<div style={{ display: 'flex' }}>
-		{Object.keys(mintTheme).map(color => (
-			<ColoredBox backGroundColor={mintTheme[color]}>
-				<p
-					key={color}
-					style={{ fontFamily: '"Courier New", Courier, monospace' }}
-				>
-					{color}
-				</p>
-			</ColoredBox>
+export const themeColorSchemes = () => (
+	<div style={{ display: 'block' }}>
+		{Object.keys(theme).map(colorScheme => (
+			<div
+				style={{
+					display: 'flex',
+					flexWrap: 'wrap',
+					justifyContent: 'center',
+					paddingBottom: 10,
+					borderBottom: '1px solid black',
+					marginBottom: 10,
+				}}
+			>
+				<p style={{
+					...font, width: '100%'
+				}}>{colorScheme}</p>
+				{Object.keys(theme[colorScheme]).map(color => {
+					const colorRGB = theme[colorScheme][color]
+
+					return (
+						<ColoredBox backGroundColor={colorRGB}>
+							<p key={colorRGB} style={{ ...font }}>
+								{color}
+							</p>
+						</ColoredBox>
+					)
+				})}
+				<br />
+			</div>
 		))}
 	</div>
 )
