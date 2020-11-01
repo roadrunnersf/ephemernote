fbb74458ec99c1f4774e6a5936dd1fb06a9e4dae
Sam Finch <roadrunnersf@gmail.com>
2020-11-01 15:38:47 +0000

    CTS: add TSFixMe alias for any
    types that need to be fixed in the furutre can be labelled as TSFixMe so they can be found later

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,10 @@ import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<Root />, document.getElementById('root'))
 
+declare global {
+	type TSFixMe = any
+}
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
